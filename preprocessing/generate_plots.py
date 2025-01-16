8ac7badbcaf77012fba452334753e8f77f3af86b
João Pedro Silveira <145754081+Joao-Pedro-Silveira@users.noreply.github.com>
2025-01-16 02:29:57 +0000

    Added new Plots

--- preprocessing/generate_plots.py
+++ preprocessing/generate_plots.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "..")) 
 metadata_file = os.path.join(root_folder, "HAM10000", "HAM10000_metadata")
@@ -41,4 +42,32 @@ plt.tight_layout()
 benign_malignant_file = os.path.join(plots_folder, "benign_vs_malignant.png")
 plt.savefig(benign_malignant_file)
 print(f"Benign vs malignant plot saved to: {benign_malignant_file}")
+plt.show()
+
+# Plot 3: Age Distribution for Each Lesion Type
+plt.figure(figsize=(12, 6))
+sns.boxplot(x='dx', y='age', data=metadata, palette='muted', order=metadata['dx'].value_counts().index)
+plt.title('Age Distribution by Lesion Type')
+plt.xlabel('Lesion Type')
+plt.ylabel('Age')
+plt.xticks(rotation=45)
+age_distribution_by_type_file = os.path.join(plots_folder, "age_distribution_by_type.png")
+plt.savefig(age_distribution_by_type_file)
+print(f"Class distribution plot saved to: {age_distribution_by_type_file}")
+plt.show()
+
+#Plot 4: Proportion of Malignant Cases by Age
+
+age_malignancy = metadata.groupby('age')['benign_malignant'].apply(lambda x: (x == 'Malignant').mean()).reset_index()
+age_malignancy.rename(columns={'benign_malignant': 'Malignant Proportion'}, inplace=True)
+
+plt.figure(figsize=(10, 6))
+plt.plot(age_malignancy['age'], age_malignancy['Malignant Proportion'], marker='o', linestyle='-', color='red')
+plt.title('Proportion of Malignant Cases by Age')
+plt.xlabel('Age')
+plt.ylabel('Proportion of Malignant Cases')
+plt.grid(alpha=0.5)
+proportion_of_malignant_by_age_file = os.path.join(plots_folder, "proportion_of_malignant_by_age.png")
+plt.savefig(proportion_of_malignant_by_age_file)
+print(f"Class distribution plot saved to: {proportion_of_malignant_by_age_file}")
 plt.show()
